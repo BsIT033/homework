7de387d6e2aef2ce8654fb1f678893a7145c3544
BsIT033 <82075865+BsIT033@users.noreply.github.com>
2021-04-12 12:12:03 +0500

    Add files via upload

---
+++ area of rectangle.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main ()
+{
+	int L,W,area;
+	printf("\nEnter length of rectangle\n");
+	scanf("%d",&L);
+	printf("\nEnter width of rectangle\n");
+	scanf("%d",&W);
+	area=L*W;
+	printf("\n Area of Rectanle = %d",area);
+	return 0;
+}
